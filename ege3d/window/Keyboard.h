9675380482476d7afa2060a6711cbe8934de9c6b
Maciej Zygmanowski <sppmacd@pm.me>
2020-11-01 12:31:28 +0100

    window: Add Keyboard key codes enumeration.
    * Handle codes in XWindowImpl
    * Add onTextEnter() event

---
+++ ege3d/window/Keyboard.h
@@ -0,0 +1,143 @@
+/*
+    EGE3d - 3D rendering engine for Hexagon
+
+    Copyright (c) 2020 Hexagon Engine
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#pragma once
+
+namespace EGE3d
+{
+
+namespace Keyboard
+{
+
+enum class Key
+{
+    Invalid,
+    Escape,
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+    F9,
+    F10,
+    F11,
+    F12,
+    Insert,
+    Delete,
+    Home,
+    End,
+    PageUp,
+    PageDown,
+    Grave,
+    N1,
+    N2,
+    N3,
+    N4,
+    N5,
+    N6,
+    N7,
+    N8,
+    N9,
+    N0,
+    NMinus,
+    Equal,
+    Backspace,
+    NumLock,
+    NumDivide,
+    NumMultiply,
+    NumSubtract,
+    Tab,
+    Q,
+    W,
+    E,
+    R,
+    T,
+    Y,
+    U,
+    I,
+    O,
+    P,
+    LeftBracket,
+    RightBracket,
+    Enter,
+    Num7,
+    Num8,
+    Num9,
+    NumAdd,
+    CapsLock,
+    A,
+    S,
+    D,
+    F,
+    G,
+    H,
+    J,
+    K,
+    L,
+    Semicolon,
+    Apostrophe,
+    Backslash,
+    Num4,
+    Num5,
+    Num6,
+    LShift,
+    Z,
+    X,
+    C,
+    V,
+    B,
+    N,
+    M,
+    Comma,
+    Period,
+    Slash,
+    RShift,
+    Num1,
+    Num2,
+    Num3,
+    NumEnter,
+    LCtrl,
+    LSuper,
+    RSuper,
+    Alt,
+    Space,
+    AltGr,
+    RCtrl,
+    Left,
+    Up,
+    Down,
+    Right,
+    Num0,
+    NumPeriod,
+
+    // keep last
+    __Count
+};
+
+};
+
+}
--- ege3d/window/XWindowImpl.cpp
+++ ege3d/window/XWindowImpl.cpp
@@ -24,7 +24,9 @@
 
 #include "XWindowImpl.h"
 
+#include <ege3d/window/Keyboard.h>
 #include <ege3d/window/Window.h>
+#include <iomanip>
 #include <iostream>
 #include <unistd.h>
 #include <X11/Xutil.h>
@@ -35,6 +37,125 @@ namespace EGE3d
 
 namespace Internal
 {
+
+Keyboard::Key xToEGE(KeySym xkey)
+{
+    switch(xkey)
+    {
+    case XK_Escape: return Keyboard::Key::Escape;
+    case XK_F1: return Keyboard::Key::F1;
+    case XK_F2: return Keyboard::Key::F2;
+    case XK_F3: return Keyboard::Key::F3;
+    case XK_F4: return Keyboard::Key::F4;
+    case XK_F5: return Keyboard::Key::F5;
+    case XK_F6: return Keyboard::Key::F6;
+    case XK_F7: return Keyboard::Key::F7;
+    case XK_F8: return Keyboard::Key::F8;
+    case XK_F9: return Keyboard::Key::F9;
+    case XK_F10: return Keyboard::Key::F10;
+    case XK_F11: return Keyboard::Key::F11;
+    case XK_F12: return Keyboard::Key::F12;
+    case XK_Insert: return Keyboard::Key::Insert;
+    case XK_Delete: return Keyboard::Key::Delete;
+    case XK_Home: return Keyboard::Key::Home;
+    case XK_End: return Keyboard::Key::End;
+    case XK_Page_Up: return Keyboard::Key::PageUp;
+    case XK_Page_Down: return Keyboard::Key::PageDown;
+    case XK_grave: return Keyboard::Key::Grave;
+    case XK_1: return Keyboard::Key::N1;
+    case XK_2: return Keyboard::Key::N2;
+    case XK_3: return Keyboard::Key::N3;
+    case XK_4: return Keyboard::Key::N4;
+    case XK_5: return Keyboard::Key::N5;
+    case XK_6: return Keyboard::Key::N6;
+    case XK_7: return Keyboard::Key::N7;
+    case XK_8: return Keyboard::Key::N8;
+    case XK_9: return Keyboard::Key::N9;
+    case XK_0: return Keyboard::Key::N0;
+    case XK_minus: return Keyboard::Key::NMinus;
+    case XK_equal: return Keyboard::Key::Equal;
+    case XK_BackSpace: return Keyboard::Key::Backspace;
+    case XK_Num_Lock: return Keyboard::Key::NumLock;
+    case XK_KP_Divide: return Keyboard::Key::NumDivide;
+    case XK_KP_Multiply: return Keyboard::Key::NumMultiply;
+    case XK_KP_Subtract: return Keyboard::Key::NumSubtract;
+    case XK_Tab: return Keyboard::Key::Tab;
+    case XK_q: return Keyboard::Key::Q;
+    case XK_w: return Keyboard::Key::W;
+    case XK_e: return Keyboard::Key::E;
+    case XK_r: return Keyboard::Key::R;
+    case XK_t: return Keyboard::Key::T;
+    case XK_y: return Keyboard::Key::Y;
+    case XK_u: return Keyboard::Key::U;
+    case XK_i: return Keyboard::Key::I;
+    case XK_o: return Keyboard::Key::O;
+    case XK_p: return Keyboard::Key::P;
+    case XK_bracketleft: return Keyboard::Key::LeftBracket;
+    case XK_bracketright: return Keyboard::Key::RightBracket;
+    case XK_Return: return Keyboard::Key::Enter;
+    case XK_KP_7: return Keyboard::Key::Num7;
+    case XK_KP_Home: return Keyboard::Key::Num7;
+    case XK_KP_8: return Keyboard::Key::Num8;
+    case XK_KP_Up: return Keyboard::Key::Num8;
+    case XK_KP_9: return Keyboard::Key::Num9;
+    case XK_KP_Page_Up: return Keyboard::Key::Num9;
+    case XK_KP_Add: return Keyboard::Key::NumAdd;
+    case XK_Caps_Lock: return Keyboard::Key::CapsLock;
+    case XK_a: return Keyboard::Key::A;
+    case XK_s: return Keyboard::Key::S;
+    case XK_d: return Keyboard::Key::D;
+    case XK_f: return Keyboard::Key::F;
+    case XK_g: return Keyboard::Key::G;
+    case XK_h: return Keyboard::Key::H;
+    case XK_j: return Keyboard::Key::J;
+    case XK_k: return Keyboard::Key::K;
+    case XK_l: return Keyboard::Key::L;
+    case XK_semicolon: return Keyboard::Key::Semicolon;
+    case XK_apostrophe: return Keyboard::Key::Apostrophe;
+    case XK_backslash: return Keyboard::Key::Backslash;
+    case XK_KP_4: return Keyboard::Key::Num4;
+    case XK_KP_Left: return Keyboard::Key::Num4;
+    case XK_KP_5: return Keyboard::Key::Num5;
+    case XK_KP_6: return Keyboard::Key::Num6;
+    case XK_KP_Right: return Keyboard::Key::Num6;
+    case XK_Shift_L: return Keyboard::Key::LShift;
+    case XK_z: return Keyboard::Key::Z;
+    case XK_x: return Keyboard::Key::X;
+    case XK_c: return Keyboard::Key::C;
+    case XK_v: return Keyboard::Key::V;
+    case XK_b: return Keyboard::Key::B;
+    case XK_n: return Keyboard::Key::N;
+    case XK_m: return Keyboard::Key::M;
+    case XK_comma: return Keyboard::Key::Comma;
+    case XK_period: return Keyboard::Key::Period;
+    case XK_slash: return Keyboard::Key::Slash;
+    case XK_Shift_R: return Keyboard::Key::RShift;
+    case XK_KP_1: return Keyboard::Key::Num1;
+    case XK_KP_End: return Keyboard::Key::Num1;
+    case XK_KP_2: return Keyboard::Key::Num2;
+    case XK_KP_Down: return Keyboard::Key::Num2;
+    case XK_KP_3: return Keyboard::Key::Num3;
+    case XK_KP_Page_Down: return Keyboard::Key::Num3;
+    case XK_KP_Enter: return Keyboard::Key::NumEnter;
+    case XK_Control_L: return Keyboard::Key::LCtrl;
+    case XK_Super_L: return Keyboard::Key::LSuper;
+    case XK_Super_R: return Keyboard::Key::RSuper;
+    case XK_Alt_L: return Keyboard::Key::Alt;
+    case XK_space: return Keyboard::Key::Space;
+    case XK_Alt_R: return Keyboard::Key::AltGr;
+    case XK_Control_R: return Keyboard::Key::RCtrl;
+    case XK_Left: return Keyboard::Key::Left;
+    case XK_Up: return Keyboard::Key::Up;
+    case XK_Down: return Keyboard::Key::Down;
+    case XK_Right: return Keyboard::Key::Right;
+    case XK_KP_0: return Keyboard::Key::Num0;
+    case XK_KP_Insert: return Keyboard::Key::Num0;
+    case XK_KP_Decimal: return Keyboard::Key::NumPeriod;
+    case XK_KP_Delete: return Keyboard::Key::NumPeriod;
+    default:
+        return Keyboard::Key::Invalid;
+    }
+}
 
 std::unique_ptr<WindowImpl> WindowImpl::make(Window* window)
 {
@@ -162,7 +283,7 @@ void XWindowImpl::handleEvent(XEvent& event)
         {
             int x = event.xmotion.x;
             int y = event.xmotion.y;
-            std::cout << "onMouseMove [" << x << "," << y << "]" << std::endl;
+            std::cout << "onMouseMove x=" << x << " y=" << y << std::endl;
         } break;
     case ButtonPress:
         {
@@ -195,12 +316,52 @@ void XWindowImpl::handleEvent(XEvent& event)
     case KeyPress:
         {
             int key = event.xkey.keycode;
-            std::cout << "onKeyPress k=" << key << std::endl;
+            bool alt = event.xkey.state & Mod1Mask;
+            bool control = event.xkey.state & ControlMask;
+            bool shift = event.xkey.state & ShiftMask;
+            bool super = event.xkey.state & Mod4Mask;
+
+            // SFML
+            Keyboard::Key egeKey;
+            for(size_t s = 0; s < 4; s++)
+            {
+                egeKey = xToEGE(XLookupKeysym(&event.xkey, s));
+                if(egeKey != Keyboard::Key::Invalid)
+                    break;
+            }
+            std::cout << "onKeyPress k=" << (int)egeKey << " a=" << alt << " c=" << control << " s=" << shift << " u=" << super << std::endl;
+
+            // SFML
+            if(!XFilterEvent(&event, None))
+            {
+                static XComposeStatus status;
+
+                // TODO: it should be dynamic in some way
+                char keyBuffer[16];
+                if(XLookupString(&event.xkey, keyBuffer, sizeof(keyBuffer), nullptr, &status))
+                {
+                    std::cout << "onTextEnter " << "str='" << keyBuffer[0] << "'" << std::endl;
+                }
+            }
         } break;
     case KeyRelease:
         {
-            int key = event.xkey.keycode;
-            std::cout << "onKeyRelease k=" << key << std::endl;
+            KeySym key = event.xkey.keycode;
+            bool alt = event.xkey.state & Mod1Mask;
+            bool control = event.xkey.state & ControlMask;
+            bool shift = event.xkey.state & ShiftMask;
+            bool super = event.xkey.state & Mod4Mask;
+
+            // SFML
+            Keyboard::Key egeKey;
+            for(size_t s = 0; s < 4; s++)
+            {
+                egeKey = xToEGE(XLookupKeysym(&event.xkey, s));
+                if(egeKey != Keyboard::Key::Invalid)
+                    break;
+            }
+
+            std::cout << "onKeyRelease k=" << (int)egeKey << " a=" << alt << " c=" << control << " s=" << shift << " u=" << super << std::endl;
         } break;
     default:
         std::cout << "Invalid event type " << event.type << std::endl;
